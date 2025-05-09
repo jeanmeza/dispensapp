a8af85543fe0009e10879bddd8880abcbf4e402f
Jean Meza <meza.jean@outlook.com>
2025-05-09 17:09:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+🥫 DispensApp
+A smart pantry inventory app to help you track what you already have before buying groceries.
+
+📝 Description
+DispensApp is a mobile application designed to help users manage their pantry by keeping track of stored food and household items. The core idea is to prevent duplicate purchases by letting users quickly check what they already have while grocery shopping.
+
+Whether you're at the grocery store or planning a shopping list from home, DispensApp makes it easy to:
+
+Track what’s in your pantry
+
+Avoid waste and overbuying
+
+Stay organized with minimal effort
+
+🚧 Project Status
+This app is currently under active development for a Master's degree course in Mobile Development.
---
+++ app/src/main/java/com/jeanmeza/dispensapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.jeanmeza.dispensapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jeanmeza.dispensapp.ui.theme.DispensAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DispensAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DispensAppTheme {
+        Greeting("Android")
+    }
+}
