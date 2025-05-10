33b78d6d2df07b8b677b23f54a063f735e78e8f6
Jean Meza <meza.jean@outlook.com>
2025-05-10 17:03:31 +0200

    refactor theme and typography with new color schemes and fonts
    Introduced detailed light and dark color schemes for high, medium, and default contrast levels. Updated typography to use the Lato font family with customizable weights and styles. Added font certificate resources for enhanced compatibility with Android’s font system.

--- app/src/main/java/com/jeanmeza/dispensapp/ui/theme/Type.kt
+++ app/src/main/java/com/jeanmeza/dispensapp/ui/theme/Type.kt
@@ -1,34 +1,43 @@
 package com.jeanmeza.dispensapp.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.jeanmeza.dispensapp.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+val Lato = FontFamily(
+    Font(R.font.lato_black, FontWeight.Black),
+    Font(R.font.lato_black_italic, FontWeight.Black, FontStyle.Italic),
+    Font(R.font.lato_bold, FontWeight.Bold),
+    Font(R.font.lato_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.lato_italic, style = FontStyle.Italic),
+    Font(R.font.lato_light, FontWeight.Light),
+    Font(R.font.lato_light_italic, FontWeight.Light, FontStyle.Italic),
+    Font(R.font.lato_regular),
+    Font(R.font.lato_thin, FontWeight.Thin),
+    Font(R.font.lato_thin, FontWeight.Thin, FontStyle.Italic),
 )
+
+// Default Material 3 typography values
+val baseline = Typography()
+
+val Typography = Typography(
+    displayLarge = baseline.displayLarge.copy(fontFamily = Lato),
+    displayMedium = baseline.displayMedium.copy(fontFamily = Lato),
+    displaySmall = baseline.displaySmall.copy(fontFamily = Lato),
+    headlineLarge = baseline.headlineLarge.copy(fontFamily = Lato),
+    headlineMedium = baseline.headlineMedium.copy(fontFamily = Lato),
+    headlineSmall = baseline.headlineSmall.copy(fontFamily = Lato),
+    titleLarge = baseline.titleLarge.copy(fontFamily = Lato),
+    titleMedium = baseline.titleMedium.copy(fontFamily = Lato),
+    titleSmall = baseline.titleSmall.copy(fontFamily = Lato),
+    bodyLarge = baseline.bodyLarge.copy(fontFamily = Lato),
+    bodyMedium = baseline.bodyMedium.copy(fontFamily = Lato),
+    bodySmall = baseline.bodySmall.copy(fontFamily = Lato),
+    labelLarge = baseline.labelLarge.copy(fontFamily = Lato),
+    labelMedium = baseline.labelMedium.copy(fontFamily = Lato),
+    labelSmall = baseline.labelSmall.copy(fontFamily = Lato),
+)
+
