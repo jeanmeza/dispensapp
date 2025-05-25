3a837435ba51cdb8494785b0eb311f6ea90c676c
Jean Meza <meza.jean@outlook.com>
2025-05-26 00:18:06 +0200

    feat: add mapping function for ItemEntity to Item model
    This commit introduces a `toModel()` extension function to the `ItemEntity` class. This function facilitates the conversion of the local database entity (`ItemEntity`) to the external data model (`Item`), which is used by other layers of the application.

--- app/src/main/java/com/jeanmeza/dispensapp/data/local/entities/ItemEntity.kt
+++ app/src/main/java/com/jeanmeza/dispensapp/data/local/entities/ItemEntity.kt
@@ -3,6 +3,7 @@ package com.jeanmeza.dispensapp.data.local.entities
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.jeanmeza.dispensapp.data.model.Item
 
 @Entity(tableName = "item")
 data class ItemEntity(
@@ -14,4 +15,15 @@ data class ItemEntity(
     val quantity: Int,
     @ColumnInfo(name = "expiry_date")
     val expiryDate: Long?
+)
+
+/**
+ * Converts the local model to the external model for use by layers external to the data layer
+ */
+fun ItemEntity.toModel() = Item(
+    id = id,
+    categoryId = categoryId,
+    name = name,
+    quantity = quantity,
+    expiryDate = expiryDate
 )
